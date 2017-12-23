cb514f88c1dfe27954673ea02512d7e6463eff71
nepeat <nepeat@users.noreply.github.com>
2017-12-22 22:54:44 -0800

    working reddit scrape

--- tools/pyshell.py
+++ tools/pyshell.py
@@ -5,46 +5,52 @@ import uuid
 from urllib.parse import urlparse
 
 import praw
+from spidercommon.regexes import onion_regex
 from spidercommon.redis import create_redis
 
 # Connections
 
-reddit = praw.Reddit(user_agent="/u/nepeat tor onion scraper")
-
-if "REDDIT_USERNAME" in os.environ and "REDDIT_PASSWORD" in os.environ:
-    reddit.login(
-        os.environ["REDDIT_USERNAME"],
-        os.environ["REDDIT_PASSWORD"]
-    )
+reddit = praw.Reddit(
+    user_agent="/u/nepeat tor onion scraper",
+    client_id=os.environ["REDDIT_CLIENT_ID"],
+    client_secret=os.environ["REDDIT_CLIENT_SECRET"],
+    username=os.environ.get("REDDIT_USERNAME", None),
+    password=os.environ.get("REDDIT_PASSWORD", None)
+)
 
 redis = create_redis()
-
-# Parsers
-
-onion_regex = re.compile(r"IMCOMPLETE", re.IGNORECASE)
 
 # CLI functions
 
 def fetch_subreddit(subreddit, top=True, limit=None):
-    sub = reddit.get_subreddit(subreddit)
-
-    for post in sub.get_hot(limit=limit):
-        check_onions(post)
-
+    sub = reddit.subreddit(subreddit)
+    getter_funcs = [sub.hot(limit=limit)]
     if top:
-        topfuncs = [
-            sub.get_top_from_all,
-            sub.get_top_from_year,
-            sub.get_top_from_month,
-            sub.get_top_from_week,
-            sub.get_top_from_day,
-        ]
-        for func in topfuncs:
-            for post in func(limit=limit):
-                check_onions(post)
+        getter_funcs.extend([
+            sub.top("all", limit=limit),
+            sub.top("year", limit=limit),
+            sub.top("month", limit=limit),
+            sub.top("week", limit=limit),
+            sub.top("day", limit=limit),
+        ])
+    for func in getter_funcs:
+        for post in func:
+            check_onions(post.url)
+            check_onions(post.selftext)
 
-def check_onions(post):
-    raise NotImplementedError()
+def check_onions(content):
+    if not content:
+        return
+
+    try:
+        content = str(content)
+    except UnicodeDecodeError:
+        pass
+
+    onions = onion_regex.findall(content)
+    for onion in onions:
+        print(f"Adding {onion} to queue.")
+        redis.sadd("torspider:urls", f"http://{onion}")
 
 if __name__ == "__main__":
     code.interact(local=locals())
