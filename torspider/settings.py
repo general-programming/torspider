93030765aa6512657417733054090da354067091
nepeat <nepeat@users.noreply.github.com>
2017-12-24 01:48:34 -0800

    um

--- torspider/settings.py
+++ torspider/settings.py
@@ -43,14 +43,14 @@ TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'Accept-Language': 'en-US,en;q=0.5',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
 }
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 SPIDER_MIDDLEWARES = {
-   'TorspiderSpiderMiddlewarescrapy.spidermiddlewares.depth.DepthMiddleware': 100,
+    'scrapy.spidermiddlewares.depth.DepthMiddleware': 100,
 }
 
 # Enable or disable downloader middlewares
