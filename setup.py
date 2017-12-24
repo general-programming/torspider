fd9c6a5123dd4db66d43ed2fea238160c39da40b
nepeat <nepeat@users.noreply.github.com>
2017-12-24 02:54:09 -0800

    zip safe is safe for us

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="torspider",
     packages=find_packages(),
-    zip_safe=False,
+    zip_safe=True,
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
 )
