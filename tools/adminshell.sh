a0dad83a321e01f984bea99ec972a195008b2d05
nepeat <nepeat@users.noreply.github.com>
2017-12-24 19:34:06 -0800

    nano in admin container

--- tools/adminshell.sh
+++ tools/adminshell.sh
@@ -3,7 +3,7 @@
 # Assuming code for the dev setup is not kept in /app/torspider, we should be fine.
 if [ -d "/app/torspider" ]; then
     # Container
-    apk add --no-cache postgresql-client redis zsh busybox-extras
+    apk add --no-cache postgresql-client redis zsh busybox-extras nano
     echo "alias redis-cli='redis-cli -h $REDIS_HOST -p $REDIS_PORT'" >> ~/.zshrc
     zsh -i
 else
