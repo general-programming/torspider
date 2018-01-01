0a2891d3a676bd2c570912dd35300ef089610726
nepeat <nepeat@users.noreply.github.com>
2018-01-01 01:34:13 -0800

    Move all the parsing code to the base.

--- torspider/spiders/base.py
+++ torspider/spiders/base.py
@@ -1,6 +1,7 @@
 import time
+import re
 from datetime import datetime
-
+import scrapy
 from scrapy_redis.spiders import RedisSpider
 
 from spidercommon.db import Domain, Page
@@ -9,6 +10,7 @@ from spidercommon.util import lock_single, md5
 
 
 class SpiderBase(RedisSpider):
+    
     allowed_domains = ['onion']
 
     spider_exclude = [
@@ -23,6 +25,8 @@ class SpiderBase(RedisSpider):
         'http://torlinkbgs6aabns.onion/'
     ]
 
+    max_parse_size_kb = 2048
+
     def setup_redis(self, *args, **kwargs):
         super().setup_redis(*args, **kwargs)
 
@@ -35,7 +39,92 @@ class SpiderBase(RedisSpider):
             #pylint: disable=E1101
             self.server.sadd("torspider:urls", self.passed_url)
 
-    def update_page_info(self, url, host, title, status_code, content, is_frontpage=False, size=0, db=None):
+    def update_page_info(self, response, db=None, follow_links: bool=False):
+        """
+            Updates the page meta information in DB.
+            Creates the DB objects if they do not exist.
+
+            Updates:
+                - Title
+                - Hostname
+                - Last status code
+                - Server header
+                - Powered By headers
+                - Linked to pages
+
+            Returns:
+                Parsed URL object and Page model object.
+        """
+
+        # Grab the title of the page.
+        title = ''
+        try:
+            title = response.css('title::text').extract_first()
+        except AttributeError:
+            pass
+        except scrapy.exceptions.NotSupported:
+            self.log(f"Skipping non-text file {response.url}")
+            return None, None
+
+        # Get tor URL "hostname"
+        parsed = ParsedURL(response.url)
+
+        self.log('Got %s (%s)' % (response.url, title))
+        is_frontpage = Page.is_frontpage_request(response.request)
+        size = len(response.body)
+        
+        page, domain = self._update_page_info(response.url, parsed.host, title, response.status, response.text, is_frontpage, size, db)
+        if not page:
+            return None, None
+
+        # Domain headers
+        if page.got_server_response:
+            if response.headers.get("Server"):
+                domain.header_server = str(response.headers.get("Server"))
+            if response.headers.get("X-Powered-By"):
+                domain.header_powered_by = str(response.headers.get("X-Powered-By"))
+            if response.headers.get("Powered-By"):
+                domain.header_powered_by = str(response.headers.get("Powered-By"))
+
+        is_text = False
+        content_type = str(response.headers.get("Content-Type"))
+        if page.got_server_response and content_type and re.match('^text/', content_type.strip()):
+            is_text = True
+        
+        # Update links_to
+        link_to_list = []
+
+        if parsed.host not in self.spider_exclude:
+            for url in response.xpath('//a/@href').extract():
+                fullurl = response.urljoin(url)
+
+                if follow_links:
+                    yield scrapy.Request(fullurl, callback=self.parse)
+
+                if page.got_server_response and Domain.is_onion_url(fullurl):
+                    try:
+                        parsed_link = ParsedURL(fullurl)
+                    except:
+                        continue
+                    link_host = parsed_link.host
+                    if parsed.host != link_host:
+                        link_to_list.append(fullurl)
+
+            self.log("link_to_list %s" % link_to_list)
+                
+            if page.got_server_response:
+                small_body = response.body[:(1024 * self.max_parse_size_kb)]
+                links_to = set()
+                for url in link_to_list:
+                    if url not in page.links_to:
+                        links_to.add(url)
+                    page.links_to = list(links_to)
+
+            db.commit()
+
+        return parsed, page
+
+    def _update_page_info(self, url: str, host: str, title: str, status_code: int, content, is_frontpage: bool=False, size: int=0, db=None):
         if not Domain.is_onion_url(url):
             return None, None
 
--- torspider/spiders/directory_spider.py
+++ torspider/spiders/directory_spider.py
@@ -24,48 +24,17 @@ class DirectorySpider(SpiderBase):
         'MAX_PAGES_PER_DOMAIN' : 2000,
         # scrapy_redis middleware
         'REDIS_START_URLS_AS_SET': True,
-        'REDIS_START_URLS_KEY': 'torspider:urls'
+        'REDIS_START_URLS_KEY': 'torspider:urls',
     }
 
     @db_session
     def parse(self, response, recent_alive_check=False, db=None):
-        MAX_PARSE_SIZE_KB = 2048
+        parsed, page = self.update_page_info(response, db, follow_links=True)
 
-        # Grab the title of the page.
-        title = ''
-        try:
-            title = response.css('title::text').extract_first()
-        except AttributeError:
-            pass
-        except scrapy.exceptions.NotSupported:
-            self.log(f"Skipping non-text file {response.url}")
-            return
-
-        # Get tor URL "hostname"
-        parsed = ParsedURL(response.url)
-
-        self.log('Got %s (%s)' % (response.url, title))
-        is_frontpage = Page.is_frontpage_request(response.request)
-        size = len(response.body)
-        
-        page, domain = self.update_page_info(response.url, parsed.host, title, response.status, response.text, is_frontpage, size, db)
         if not page:
             return
 
-        # Domain headers
-        if page.got_server_response:
-            if response.headers.get("Server"):
-                domain.header_server = str(response.headers.get("Server"))
-            if response.headers.get("X-Powered-By"):
-                domain.header_powered_by = str(response.headers.get("X-Powered-By"))
-            if response.headers.get("Powered-By"):
-                domain.header_powered_by = str(response.headers.get("Powered-By"))
-
-        is_text = False
-        content_type = str(response.headers.get("Content-Type"))
-        if page.got_server_response and content_type and re.match('^text/', content_type.strip()):
-            is_text = True
-
+        # XXX Make this code work.
         """ See how useful random path checking can be later.
         # Add some randomness to the check
         path_event_horizon = datetime.now() - timedelta(days=14 + random.randint(0, 14))
@@ -83,30 +52,3 @@ class DirectorySpider(SpiderBase):
             commit()
             yield scrapy.Request(domain.construct_url("/description.json"), callback=self.description_json)
         """
-        
-        link_to_list = []
-
-        if parsed.host not in DirectorySpider.spider_exclude:
-            for url in response.xpath('//a/@href').extract():
-                fullurl = response.urljoin(url)
-                yield scrapy.Request(fullurl, callback=self.parse)
-                if page.got_server_response and Domain.is_onion_url(fullurl):
-                    try:
-                        parsed_link = ParsedURL(fullurl)
-                    except:
-                        continue
-                    link_host = parsed_link.host
-                    if parsed.host != link_host:
-                        link_to_list.append(fullurl)
-
-            self.log("link_to_list %s" % link_to_list)
-                
-            if page.got_server_response:
-                small_body = response.body[:(1024 * MAX_PARSE_SIZE_KB)]
-                links_to = set()
-                for url in link_to_list:
-                    if url not in page.links_to:
-                        links_to.add(url)
-                    page.links_to = list(links_to)
-
-            db.commit()
