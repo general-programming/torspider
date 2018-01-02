e41c68304bd344ca5967e7aa46fc1af94a9cc71d
nepeat <nepeat@users.noreply.github.com>
2018-01-02 02:25:33 -0800

    Refine startup script for crawlers and update tor submodule.

--- torspider/__main__.py
+++ torspider/__main__.py
@@ -1,13 +1,22 @@
 from scrapy.crawler import CrawlerRunner
-from twisted.internet import reactor
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+from twisted.internet import defer, reactor
+
 from torspider.spiders.directory_spider import DirectorySpider
 from torspider.spiders.link_spider import LinkSpider
 
-if __name__ == "__main__":
-    runner = CrawlerRunner()
+configure_logging(install_root_handler=False)
+runner = CrawlerRunner(get_project_settings())
+
+@defer.inlineCallbacks
+def crawl():
     runner.crawl(DirectorySpider)
     runner.crawl(LinkSpider)
     d = runner.join()
     d.addBoth(lambda _: reactor.stop())
+    yield d
 
+if __name__ == "__main__":
+    crawl()
     reactor.run()
