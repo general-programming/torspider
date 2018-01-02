2cfbd55e75ed0986ec1e0a38e51d300aab367b81
nepeat <nepeat@users.noreply.github.com>
2018-01-02 01:41:30 -0800

    Tor2web crawling and bump up single link timeout to 20s.

--- torspider/spiders/link_spider.py
+++ torspider/spiders/link_spider.py
@@ -12,7 +12,7 @@ class LinkSpider(SpiderBase):
         # Scrapy
         'DOWNLOAD_MAXSIZE': (1024 * 1024) * 2,
         # This is assuming that the spider has a fast connection to Tor.
-        "DOWNLOAD_TIMEOUT": 10,
+        "DOWNLOAD_TIMEOUT": 20,
         # Middleware
         'MAX_PAGES_PER_DOMAIN' : -1,
         # scrapy_redis middleware
